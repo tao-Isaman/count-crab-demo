0f5d121bbdfa06743ff51087da11692d39845fbf
Isaman sangbumrung <38631981+tao-Isaman@users.noreply.github.com>
2023-06-29 09:21:44 +0000

    fix ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import ClassifyPage from "./pages/classify";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-6">
       <ClassifyPage/>
     </main>
   )
